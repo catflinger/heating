ae635c1710948bf1b658273e213545e7693ce3b9
Paul Drury <paul@drurys.org>
2018-10-01 21:36:16 +0100

    fixed bugs with logging scheduler

--- src/logger/logger.ts
+++ src/logger/logger.ts
@@ -16,27 +16,29 @@ import {
 @injectable()
 export class Logger implements ILogger {
 
-    @inject(INJECTABLES.Controller)
-    private controller: IController;
-
-    @inject(INJECTABLES.Environment)
-    private envController: IEnvironment;
-
     private loggingJob: Job;
     private housekeepingJob: Job;
     private logFileName: string;
     private lock: boolean = false;
 
-    constructor(@inject(INJECTABLES.ControllerSettings) private settings: IControllerSettings) {
+    constructor(
+        @inject(INJECTABLES.Controller) private controller: IController,
+        @inject(INJECTABLES.ControllerSettings) private settings: IControllerSettings,
+        @inject(INJECTABLES.Environment) private envController: IEnvironment) {
+
         this.logFileName = path.join(this.settings.logDir, "heatinglog-current.csv");
     }
 
     public start(): void {
         // schedule log every 5 minutes
-        this.loggingJob = scheduleJob("00 00,05,10,15,20,25,30,35,40,45,50,55 * * *", this.writeLogEntry);
+        this.loggingJob = scheduleJob("00,05,10,15,20,25,30,35,40,45,50,55 * * * *", () => {
+            this.writeLogEntry();
+        });
 
         // schedule housekeep at just after midnight every day
-        this.housekeepingJob = scheduleJob("02 00 * * *", this.writeLogEntry);
+        this.housekeepingJob = scheduleJob("02 00 * * *", () => {
+            this.writeLogEntry();
+        });
     }
 
     public stop(): void {
@@ -56,56 +58,65 @@ export class Logger implements ILogger {
     }
 
     public writeLogEntry(): void {
-        if (!this.lock) {
-            const control: ControlStateSnapshot = this.controller.getSnapshot();
-            const env: SensorSnapshot[] = this.envController.getSnapshot();
 
-            const data: string[] = [];
+        try {
+            if (!this.lock) {
+                const control: ControlStateSnapshot = this.controller.getSnapshot();
+                const env: SensorSnapshot[] = this.envController.getSnapshot();
 
-            data.push(moment().format("YYYY-MM-DD hh:mm:ss"));
-            data.push(control.heating ? "1" : "0");
-            data.push(control.hotWater ? "1" : "0");
-            data.push(env.find((s) => s.id === "hw").reading.toString());
-            data.push(env.find((s) => s.id === "bedroom").reading.toString());
-            data.push(env.find((s) => s.id === "loft").reading.toString());
-            data.push(env.find((s) => s.id === "garage").reading.toString());
+                const data: string[] = [];
 
-            const entry: string = data.join(",") + "\n";
+                data.push(moment().format("YYYY-MM-DD hh:mm:ss"));
+                data.push(control.heating ? "1" : "0");
+                data.push(control.hotWater ? "1" : "0");
+                data.push(env.find((s) => s.id === "hw").reading.toString());
+                data.push(env.find((s) => s.id === "bedroom").reading.toString());
+                data.push(env.find((s) => s.id === "loft").reading.toString());
+                data.push(env.find((s) => s.id === "garage").reading.toString());
 
-            fs.appendFile(
-                this.getLogfileName(),
-                entry,
-                "utf-8",
-                (err) => {
-                    if (err) {
-                        // should this be reported somewhere?
-                    }
-                });
+                const entry: string = data.join(",") + "\n";
+
+                fs.appendFile(
+                    this.getLogfileName(),
+                    entry,
+                    "utf-8",
+                    (err) => {
+                        if (err) {
+                            // should this be reported somewhere?
+                        }
+                    });
+            }
+        } catch {
+            // TO DO: where to report this?
         }
     }
 
     public housekeep(): void {
 
         // TO DO: remove very old log files
+        try {
 
-        const currentName: string = this.getLogfileName();
-        const newName: string = moment().format("YYYYMMDD-hhmmss") + ".csv";
+            const currentName: string = this.getLogfileName();
+            const newName: string = moment().format("YYYYMMDD-hhmmss") + ".csv";
 
-        this.lock = true;
+            this.lock = true;
 
-        fs.access("", fs.constants.F_OK, (err1) => {
-            if (!err1) {
-                fs.rename(currentName, newName, (err2) => {
-                    if (err2) {
-                        // TO DO: where to report this?
-                    }
-                    this.lock = false;
-                });
-            } else {
-                // TO DO: where to report this?
-            }
+            fs.access("", fs.constants.F_OK, (err1) => {
+                if (!err1) {
+                    fs.rename(currentName, newName, (err2) => {
+                        if (err2) {
+                            // TO DO: where to report this?
+                        }
+                        this.lock = false;
+                    });
+                } else {
+                    // TO DO: where to report this?
+                }
 
-            this.lock = false;
-        });
+                this.lock = false;
+            });
+        } catch {
+            // TO DO: where to report this?
+        }
     }
 }
